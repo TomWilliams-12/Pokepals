04696965d2f65060578b0ca49cee851e88955830
TomWilliams-12 <williams.t17@gmail.com>
2020-08-16 09:38:01 +0100

    fixed bugs with search function

--- src/Form.js
+++ src/Form.js
@@ -12,14 +12,19 @@ const Form = () => {
   const [search, setSearch] = useState("");
 
   const fetchPokemon = () => {
-    MyPokedex.getPokemonByName(search) // with Promise
-      .then((response) => {
-        setPokemon(response);
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log("There was an ERROR: ", error);
-      });
+    if (search === "") {
+      alert("you need to search for a pokemon");
+    } else {
+      MyPokedex.getPokemonByName(search) // with Promise
+        .then((response) => {
+          setPokemon(response);
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log("There was an ERROR: ", error);
+          alert("this pokemon doesnt exist");
+        });
+    }
   };
 
   return (
